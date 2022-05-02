52058b76a02d2db5b0e73bbae3f8a276ae0b4e63
Pedro Lima <pedrohdlima@gmail.com>
2022-05-03 00:04:55 +0100

    Commit#2 Login + Biometria

--- LimaRent/views/Login.js
+++ LimaRent/views/Login.js
@@ -10,6 +10,46 @@ export default function Login({ navigation }) {
   const [password, setPassword] = useState(null);
   const [login, setLogin] = useState(null);
 
+  useEffect(() => {
+    verifyLogin();
+  }, []);
+
+  useEffect(() => {
+    if (login === true) {
+      biometric();
+    }
+  }, [login]);
+
+  //Verifica se o usuário já possui algum login
+  async function verifyLogin() {
+    let response = await AsyncStorage.getItem('userData');
+    let json = await JSON.parse(response);
+    if (json !== null) {
+      setUser(json.name);
+      setPassword(json.password);
+      setLogin(true);
+    }
+  }
+
+  //Biometria
+  async function biometric() {
+    let compatible = await LocalAuthentication.hasHardwareAsync();
+    if (compatible) {
+      let biometricRecords = await LocalAuthentication.isEnrolledAsync();
+      if (!biometricRecords) {
+        alert('Biometria não cadastrada');
+      } else {
+        let result = await LocalAuthentication.authenticateAsync();
+        if (result.success) {
+          sendForm();
+        } else {
+          setUser(null);
+          setPassword(null);
+        }
+      }
+    }
+  }
+
   //Envio do formulário de login
   async function sendForm() {
     let response = await fetch('http://192.168.1.11:3000/login', {
