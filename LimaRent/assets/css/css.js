091f79b70fe3004b1701053676672b3faa19a640
Pedro Lima <pedrohdlima@gmail.com>
2022-05-22 22:58:54 +0100

    Commit#3 Slideshow + MenuNav

--- LimaRent/assets/css/css.js
+++ LimaRent/assets/css/css.js
@@ -1,42 +1,42 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 // #30e210
 
 const css = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   container2: {
     flex: 1,
-    flexDirection: "row",
-    backgroundColor: "#000",
-    alignItems: "center",
-    justifyContent: "center",
+    flexDirection: 'row',
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   button__home: {
     marginRight: 40,
   },
   darkbg: {
-    backgroundColor: "#333",
+    backgroundColor: '#333',
   },
   login__icon: {
     marginBottom: 20,
   },
-  login__msg: (text = "none") => ({
-    fontWeight: "bold",
+  login__msg: (text = 'none') => ({
+    fontWeight: 'bold',
     fontSize: 22,
-    color: "red",
+    color: 'red',
     marginBottom: 15,
     display: text,
-    alignSelf: "center",
+    alignSelf: 'center',
   }),
   login__form: {
-    width: "80%",
+    width: '80%',
   },
   login__input: {
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     fontSize: 19,
     padding: 7,
     marginBottom: 5,
@@ -44,15 +44,43 @@ const css = StyleSheet.create({
   },
   login__button: {
     padding: 12,
-    backgroundColor: "#30e210",
-    alignSelf: "center",
+    backgroundColor: '#30e210',
+    alignSelf: 'center',
     borderRadius: 5,
     marginTop: 10,
   },
   login__buttonText: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
     fontSize: 22,
-    color: "#333",
+    color: '#333',
+  },
+
+  round_btn: {
+    marginTop: 20,
+    width: '30%',
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 100,
+    backgroundColor: '#99FF33',
+    flexDirection: 'row',
+  },
+
+  round_btnRow: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  round_btnText: {
+    fontSize: 23,
+    color: '#000',
+  },
+
+  background_Entry: {
+    flex: 1,
+    backgroundColor: '#FFF',
   },
 });
 
